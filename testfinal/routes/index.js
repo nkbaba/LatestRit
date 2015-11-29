d735ebce3f036ca09cf385ed13d7a22d256ac242
Apurva Tripathi <Apurva Tripathi>
2015-11-29 07:11:37 -0500

    Updated with the display user funcationality.
    There is some problem with updating user that needs to be solved. You can
    look into the index.js file bottom and try to fix the error in the query.

--- testfinal/routes/index.js
+++ testfinal/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var global;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Find Tweets Project' });
@@ -25,7 +27,7 @@ router.post('/search', function(req, res) {
     // Get our form values. These rely on the "name" attributes
     var userName = req.body.username;
     //var userEmail = req.body.useremail;
-
+    global = userName;
     // Set our collection
     var collection = db.get('tweets');
 
@@ -47,5 +49,43 @@ router.post('/search', function(req, res) {
         
     });
 });
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('tweets');
+    var query= {"text" : new RegExp(global,"i")};
+
+    // Submit to the DB
+    collection.find(query, function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * Update to Comment
+ */
+router.post('/commentuser', function(req, res) {
+    //response.setHeader('Access-Control-Allow-Origin', '*');
+    var db = req.db;
+    console.log("comment working at server level");
+
+    var collection = db.get('tweets');
+    var userID = req.body.id1;
+    var userComment= req.body.comment;
+    console.log(userID);
+    console.log(userComment);
+    var query= {"id": userID};
+    var query2={"$set": {"comment" : "Hello1"}};
+    //collection.update({'id' : userID}, {$set: {"comment": userComment}}, false, true, function(err) {
+        //res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    collection.update(query,query2, false, false, function(err){
+    //var err=null;
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+    });
+
 
 module.exports = router;
